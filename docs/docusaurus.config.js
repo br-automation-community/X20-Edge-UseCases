ca8eefc2a69a50114a47dd831d175b9b563a6c85
Haukur Kristinsson <haukur.kristinsson@br-automation.com>
2025-03-20 09:24:12 +0100

    update baseUrl for github pages

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -18,7 +18,7 @@ const config = {
   url: 'https://brdk-github.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/X20-Edge/',
+  baseUrl: '/X20-Edge-UseCases/',
   trailingSlash: false,
 
   // GitHub pages deployment config.
